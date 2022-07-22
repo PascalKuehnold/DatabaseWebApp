0e3d6039e79391f45f12b6938aaeba9aec5cb982
Pascal <Pascal.Kuehnold@gmail.com>
2022-07-22 04:54:27 +0200

    added customer entity class for database work

---
+++ src/main/java/de/pascalkuehnold/databasewebapp/entities/Customer.java
@@ -0,0 +1,78 @@
+package de.pascalkuehnold.databasewebapp.entities;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    //region Fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    //endregion Fields
+
+    //region Constructors
+    public Customer(){
+        //empty constructor
+    }
+    //endregion Constructors
+
+    //region Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    //endregion
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
